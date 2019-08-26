b86c742350d72e0e1860d57e1867dea87a4b1fdb
Carl Hain <crhain@gmail.com>
2019-08-25 21:07:22 -0500

    Add text input, label, and some styling to SearchBar component; add some styling to App component to create a gutter on each side of the application

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,16 @@ import { directive } from '@babel/types';
 
 class SearchBar extends React.Component {
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
