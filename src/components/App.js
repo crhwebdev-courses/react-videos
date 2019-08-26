f67aa93d19c3501b33021d138ffe05e596c5ee4c
Carl Hain <crhain@gmail.com>
2019-08-26 14:48:56 -0500

    Refactor App to set selectedVideo state item when calling onVideoSelect click handler

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
   };
 
   onVideoSelect = video => {
-    console.log('From the App!', video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
