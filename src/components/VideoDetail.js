37b561e51b131f729aec9163d1976694f72deb7b
Carl Hain <crhain@gmail.com>
2019-08-26 15:59:21 -0500

    VideoDetail shows selected video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,8 +5,20 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
 
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe
+          width="560"
+          height="315"
+          src={videoSrc}
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
