b579bca06ac4760d0e4f5b2a54d345f6cb91de49
Carl Hain <crhain@gmail.com>
2019-08-25 20:56:28 -0500

    Create SearchBar component and wire it up to App.js

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { directive } from '@babel/types';
+
+class SearchBar extends React.Component {
+  render() {
+    return <div>SearchBar</div>;
+  }
+}
+
+export default SearchBar;
